97cd6c1a767adc140feafbf838c615c827f30c9e
nickpavini <nickpavini@gmail.com>
2021-03-20 07:20:06 -0700

    Base commit for API. Need to set-up API to have actions also. So accounts/login accounts/5/name, etc... Assuming I am doing a decent convention

---
+++ Models/Account.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SafeAccountsAPI
+{
+    public class Account
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
+    }
+}
