dfa8481bcf2acfb22fa6f8e09297f0d1b5d3b784
mehrwegflasche <vhokayy@gmail.com>
2021-05-01 10:16:09 +0200

    Add Unit Test Projects and restructure folders

---
+++ SafeAccountsAPI.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace SafeAccountsAPI.UnitTests {
+	public class HelpersTest {
+		[SetUp]
+		public void Setup() {
+		}
+
+		[Test]
+		public void Test1() {
+			Assert.Pass();
+		}
+	}
+}
