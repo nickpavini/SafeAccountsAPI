4e9fd78673db57141a4b0713a47df07b70de739a
nickpavini <36856910+nickpavini@users.noreply.github.com>
2021-06-13 21:42:50 -0700

    Update Startup.cs

--- SafeAccountsAPI/Startup.cs
+++ SafeAccountsAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using SafeAccountsAPI.Data;
+
 namespace SafeAccountsAPI
 {
     public class Startup
