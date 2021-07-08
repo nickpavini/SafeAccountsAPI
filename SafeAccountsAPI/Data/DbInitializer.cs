d92c085658426f9ec46eebaa23c7b5cffe02b76c
nickpavini <nickpavini@gmail.com>
2021-07-08 08:58:21 -0700

    set default account data to be encrypted aes data based on default user password of 'useless', this helps testing as now the ui can actually decrypt the default data as it would in practice

--- SafeAccountsAPI/Data/DbInitializer.cs
+++ SafeAccountsAPI/Data/DbInitializer.cs
@@ -83,101 +83,101 @@ namespace SafeAccountsAPI.Data
                 {
                     new Account {
                         UserID=1,
-                        Title=Encoding.UTF8.GetBytes("gmail"),
-                        Login=Encoding.UTF8.GetBytes("johndoe"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("9625B5E80B10566585FF0DA0B856AD68"),
+                        Login=HelperMethods.HexStringToByteArray("C5394419056C9081A2FAF6CE6AF8ACB0"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=1,
-                        Title=Encoding.UTF8.GetBytes("yahoo"),
-                        Login=Encoding.UTF8.GetBytes("johndoe"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("91CB16829F918EA24FA9857664E25A27"),
+                        Login=HelperMethods.HexStringToByteArray("C5394419056C9081A2FAF6CE6AF8ACB0"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=2,
-                        Title=Encoding.UTF8.GetBytes("paypal"),
-                        Login=Encoding.UTF8.GetBytes("edwinmay"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("F67714B3ACC7A6FD485B55FC65C75F66"),
+                        Login=HelperMethods.HexStringToByteArray("70F663B0D722D99DF67B0A9BD654FC93"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=2,
-                        Title=Encoding.UTF8.GetBytes("zoom"),
-                        Login=Encoding.UTF8.GetBytes("edwinmay"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("E8A18F66E16ABA40F2AA92B1227D1BF5"),
+                        Login=HelperMethods.HexStringToByteArray("70F663B0D722D99DF67B0A9BD654FC93"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=3,
-                        Title=Encoding.UTF8.GetBytes("chase"),
-                        Login=Encoding.UTF8.GetBytes("lucyvale"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("27041EC622E17377F7E5B822D2940611"),
+                        Login=HelperMethods.HexStringToByteArray("C89B1CFEA640A606A9DAB0FAD7D47703"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=3,
-                        Title=Encoding.UTF8.GetBytes("netflix"),
-                        Login=Encoding.UTF8.GetBytes("lucyvale"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("DA5932D26A6AF3072DDBAC82E13F6AD0"),
+                        Login=HelperMethods.HexStringToByteArray("C89B1CFEA640A606A9DAB0FAD7D47703"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=4,
-                        Title=Encoding.UTF8.GetBytes("hulu"),
-                        Login=Encoding.UTF8.GetBytes("pamwillis"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("9FC71B6C5345C1C5A9DDBDAC3A7FECB5"),
+                        Login=HelperMethods.HexStringToByteArray("9B09A134F0A80D163FE87DB4DB9F4430"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=4,
-                        Title=Encoding.UTF8.GetBytes("amazon"),
-                        Login=Encoding.UTF8.GetBytes("pamwillis"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("9CFF0E4F9407B0EB81366AB06B687AF0"),
+                        Login=HelperMethods.HexStringToByteArray("9B09A134F0A80D163FE87DB4DB9F4430"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=5,
-                        Title=Encoding.UTF8.GetBytes("spotify"),
-                        Login=Encoding.UTF8.GetBytes("gamestonk"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("301712F5E57EFD4AC79F62B26F79A654"),
+                        Login=HelperMethods.HexStringToByteArray("8CCFED303762BEB13C916CDD40354624"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
                     new Account {
                         UserID=5,
-                        Title=Encoding.UTF8.GetBytes("bestbuy"),
-                        Login=Encoding.UTF8.GetBytes("gamestonk"),
-                        Password=Encoding.UTF8.GetBytes("useless"),
-                        Url=Encoding.UTF8.GetBytes("testurl.com"),
-                        Description=Encoding.UTF8.GetBytes("description..."),
+                        Title=HelperMethods.HexStringToByteArray("06AEBF7F5BDE9A3BF468D91BE81C032D"),
+                        Login=HelperMethods.HexStringToByteArray("8CCFED303762BEB13C916CDD40354624"),
+                        Password=HelperMethods.HexStringToByteArray("231D60897ACF5DF1DA18F9D9092B50F1"),
+                        Url=HelperMethods.HexStringToByteArray("0088D5E4F29C898BEEA3535D770B4AFA"),
+                        Description=HelperMethods.HexStringToByteArray("38579F1B9D124E83694AB75716E97E1C"),
                         LastModified=DateTime.Now.ToString(),
                         IsFavorite=false
                     },
@@ -199,20 +199,20 @@ namespace SafeAccountsAPI.Data
                 // add base folders
                 Folder[] base_folds = new Folder[]
                 {
-                    new Folder { UserID=1, FolderName=Encoding.UTF8.GetBytes("Folder"), HasChild =true },
-                    new Folder { UserID=2, FolderName=Encoding.UTF8.GetBytes("Folder"), HasChild=true },
-                    new Folder { UserID=3, FolderName=Encoding.UTF8.GetBytes("Folder"), HasChild=true },
-                    new Folder { UserID=4, FolderName=Encoding.UTF8.GetBytes("Folder"), HasChild=true },
-                    new Folder { UserID=5, FolderName=Encoding.UTF8.GetBytes("Folder"), HasChild=true }
+                    new Folder { UserID=1, FolderName=HelperMethods.HexStringToByteArray("F6E3C1C45DDCC44CB50355BC6EE1FD08"), HasChild =true },
+                    new Folder { UserID=2, FolderName=HelperMethods.HexStringToByteArray("F6E3C1C45DDCC44CB50355BC6EE1FD08"), HasChild=true },
+                    new Folder { UserID=3, FolderName=HelperMethods.HexStringToByteArray("F6E3C1C45DDCC44CB50355BC6EE1FD08"), HasChild=true },
+                    new Folder { UserID=4, FolderName=HelperMethods.HexStringToByteArray("F6E3C1C45DDCC44CB50355BC6EE1FD08"), HasChild=true },
+                    new Folder { UserID=5, FolderName=HelperMethods.HexStringToByteArray("F6E3C1C45DDCC44CB50355BC6EE1FD08"), HasChild=true }
                 };
 
                 Folder[] sub_folds = new Folder[]
                 {
-                    new Folder { UserID=1, FolderName=Encoding.UTF8.GetBytes("Sub-Folder"), HasChild=false, ParentID=5 },
-                    new Folder { UserID=2, FolderName=Encoding.UTF8.GetBytes("Sub-Folder"), HasChild=false, ParentID=4 },
-                    new Folder { UserID=3, FolderName=Encoding.UTF8.GetBytes("Sub-Folder"), HasChild=false, ParentID=3 },
-                    new Folder { UserID=4, FolderName=Encoding.UTF8.GetBytes("Sub-Folder"), HasChild=false, ParentID=2 },
-                    new Folder { UserID=5, FolderName=Encoding.UTF8.GetBytes("Sub-Folder"), HasChild=false, ParentID=1 }
+                    new Folder { UserID=1, FolderName=HelperMethods.HexStringToByteArray("8C9628798BCB01B195B5343BE70E44DB"), HasChild=false, ParentID=5 },
+                    new Folder { UserID=2, FolderName=HelperMethods.HexStringToByteArray("8C9628798BCB01B195B5343BE70E44DB"), HasChild=false, ParentID=4 },
+                    new Folder { UserID=3, FolderName=HelperMethods.HexStringToByteArray("8C9628798BCB01B195B5343BE70E44DB"), HasChild=false, ParentID=3 },
+                    new Folder { UserID=4, FolderName=HelperMethods.HexStringToByteArray("8C9628798BCB01B195B5343BE70E44DB"), HasChild=false, ParentID=2 },
+                    new Folder { UserID=5, FolderName=HelperMethods.HexStringToByteArray("8C9628798BCB01B195B5343BE70E44DB"), HasChild=false, ParentID=1 }
                 };
 
                 foreach (Folder fold in base_folds) { context.Folders.Add(fold); } // add each account to the table
